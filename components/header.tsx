7a7d64386148bd52e2391c6e6e77b7110890e8e1
yoh <yokeus2000@gmail.com>
2024-11-06 22:20:00 +0900

    #3 テーマ切り替えボタンを追加

--- components/header.tsx
+++ components/header.tsx
@@ -1,12 +1,15 @@
-import Link from "next/link";
 import { SidebarTrigger } from "./ui/sidebar";
 import HeaderAuth from "./header-auth";
+import { ThemeSwitcher } from "./theme-switcher";
 
 export function Header() {
   return (
     <header className="w-full flex items-center justify-between border-b border-b-foreground/10 h-16 p-6">
       <SidebarTrigger />
-      <HeaderAuth />
+      <div className="flex gap-3">
+        <HeaderAuth />
+        <ThemeSwitcher />
+      </div>
     </header>
   );
 }
